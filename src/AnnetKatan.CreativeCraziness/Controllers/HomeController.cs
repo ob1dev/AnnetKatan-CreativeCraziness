c65d22fbcc5a9cd5afb577e62001f7f68e9565ee
Oleg Burov <oleg.burov@outlook.com>
2016-02-15 22:59:02 -0800

    Initial commit.

---
+++ src/AnnetKatan.CreativeCraziness/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace AnnetKatan.CreativeCraziness.Controllers
+{
+  public class HomeController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Error()
+    {
+      return View();
+    }
+  }
+}
